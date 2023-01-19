d66143ef6233d8164dbe151b2ebbeb27ff4fa790
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-18 21:34:53 -0600

    Agregar archivos de proyecto.

---
+++ 01-01-LINQ-to_objects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_01_LINQ_to_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
